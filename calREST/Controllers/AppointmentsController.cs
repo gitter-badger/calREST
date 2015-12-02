1a640d23ca990141b795265c4561a5e55c938799
Halil <halil.ibrahim.karaca@gmail.com>
2015-12-02 17:36:42 +0100

    Added Account controller to register a user. Added Appointments controller using EF code first and referenced the class with aspnet users.

---
+++ calREST/Controllers/AppointmentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using calREST.Models;
+
+namespace calREST.Controllers
+{
+    public class AppointmentsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Appointments
+        public IQueryable<Appointment> GetAppointments()
+        {
+            return db.Appointments;
+        }
+
+        // GET: api/Appointments/5
+        [ResponseType(typeof(Appointment))]
+        public async Task<IHttpActionResult> GetAppointment(int id)
+        {
+            Appointment appointment = await db.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(appointment);
+        }
+
+        // PUT: api/Appointments/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutAppointment(int id, Appointment appointment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != appointment.AppointmentId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(appointment).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Appointments
+        [ResponseType(typeof(Appointment))]
+        public async Task<IHttpActionResult> PostAppointment(Appointment appointment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Appointments.Add(appointment);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = appointment.AppointmentId }, appointment);
+        }
+
+        // DELETE: api/Appointments/5
+        [ResponseType(typeof(Appointment))]
+        public async Task<IHttpActionResult> DeleteAppointment(int id)
+        {
+            Appointment appointment = await db.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            db.Appointments.Remove(appointment);
+            await db.SaveChangesAsync();
+
+            return Ok(appointment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return db.Appointments.Count(e => e.AppointmentId == id) > 0;
+        }
+    }
+}
---
+++ calREST/Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace calREST.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentId { get; set; }
+        public string Content { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
